34bbc6c5c22433253ea9a7f80fff55339fc379e6
Shathik Khan <106966638+Shathik131@users.noreply.github.com>
2023-09-26 19:39:28 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*============== tying annimation ==============*/
+
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Full Stack Developer",
+    "Devops Developer",
+    "YouTuber",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
